a73f0ce37f0de8d80d3fa6e4fcac595c0df6a5d3
TioMigue <65309569+TioMigue@users.noreply.github.com>
2020-06-21 16:57:34 -0400

    master

---
+++ UEstadoAnimal.php
@@ -0,0 +1,92 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="contenedor">
+        <!-- Seccion arriba -->
+        <div class="arriba">
+            <div class="login">
+
+            </div>
+            <div class="contenedor-arriba">
+
+            </div>
+        </div>
+        <!-- Seccion media -->
+        <div class="medio">
+            <div class="anunciosD">
+                <div class="anuncioD"></div>
+                <div class="anuncioD"></div>
+            </div>
+            <div class="anunciosI">
+                <div class="anuncioI"></div>
+                <div class="anuncioI"></div>
+            </div>
+            <div class="contenedor-medio-usuario">
+                <div class="contenido">
+                    <form action="" method="POST">
+                        <div class="Menu-Medio">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Home" value="Home">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Catalogo" value="Catalogo">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Animales" value="Animales">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Multimedia" value="Multimedia">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Historial" value="Historial">
+                            <input type="submit" class="btn_Report" name="btn_Error" value="Error">
+                        </div>
+                    </form>
+                    <div class="Datos-Pag2">
+                        <form action="">
+                            <table class="tablaEstado">
+                                <tr class="trEstado">
+                                    <td class="tdImgEstado"><img class="testImg" src="img/gatofrente.jpg" alt=""></td>
+                                    <td class="tdDatosEstado">
+                                        <table class="tablaDatos">
+                                            <tr class="trEstado">
+                                                <td class="tdEstadoAlimento">
+                                                    <strong>Estado comida: </strong>
+                                                    <select class="selectComida"name="" id="">
+                                                        <option value="">Efectivo</option>
+                                                        <option value="">WebPay</option>
+                                                        <option value="">PayPal</option>
+                                                    </select>
+                                                </td>
+                                            </tr>
+                                            <tr class="trEstado">
+                                                <td class="tdEstadoChequeo">
+                                                    <strong>Chequeos Medicos</strong>
+                                                </td>
+                                            </tr>
+                                            <tr class="trEstado">
+                                                <td class="tdEstadoChequeos">
+                                                    <div class="ChequeosMedicos">Algo</div>
+
+                                                </td>
+                                            </tr>
+                                        </table></td>
+                                </tr>
+                                <tr class="trEstado">
+                                    <td class="tdDescripcionEstado"><strong>Descripcion del animal</strong></td>
+                                </tr>
+                                <tr class="trEstado">
+                                    <td class="tdDescripcionLugar"><strong>Lugar</strong></td>
+                                </tr>
+                            </table>
+
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Seccion abajo -->
+    </div>
+</body>
+</html>
---
+++ URetirarAnimal.php
@@ -0,0 +1,74 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="contenedor">
+        <!-- Seccion arriba -->
+        <div class="arriba">
+            <div class="login">
+
+            </div>
+            <div class="contenedor-arriba">
+
+            </div>
+        </div>
+        <!-- Seccion media -->
+        <div class="medio">
+            <div class="anunciosD">
+                <div class="anuncioD"></div>
+                <div class="anuncioD"></div>
+            </div>
+            <div class="anunciosI">
+                <div class="anuncioI"></div>
+                <div class="anuncioI"></div>
+            </div>
+            <div class="contenedor-medio-usuario">
+                <div class="contenido">
+                    <form action="" method="POST">
+                        <div class="Menu-Medio">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Home" value="Home">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Catalogo" value="Catalogo">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Animales" value="Animales">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Multimedia" value="Multimedia">
+                            <input type="submit" class="btn_MenuUsuario" name="btn_Historial" value="Historial">
+                            <input type="submit" class="btn_Report" name="btn_Error" value="Error">
+                        </div>
+                    </form>
+                    <div class="Datos-Pag2">
+                        <form action="">
+                            <table class="tablaRetirarAnimal">
+                                <tr class="trRetirarAnimal">
+                                    <td class="tdImgRetirarAnimal"><img class="imgRetirarAnimal" src="img/gatofrente.jpg" alt=""></td>
+                                    <td class="tdDatosRetirarAnimal">
+                                        <table class="tablaDatosRetirarAnimal">
+                                            <tr class="trRetirarAnimal">
+                                                <td class="tdSueldoRetirarAnimal"><strong>Subir Archivos de sueldo</strong><input type="file"></td>
+                                            </tr>
+                                            <tr class="trRetirarAnimal">
+                                                <td class="tdHabitadRetirarAnimal"><strong>Subir Archivos de habitad</strong><input type="file"></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr class="trRetirarAnimal">
+                                    <td class="tdNotaRetirarAnimal"><strong>Nota: Segun el estado financiero y las condiciones que ofrece para tener al animal en una habitad aceptable, se le enviara una confirmacion a su correo</strong></td>
+                                    <td class="tdBtnsRetirarAnimal"><input class="btn_RetirarAnimal" type="submit" value="Volver"><input class="btn_RetirarAnimal" type="submit" value="Retirar"></td>
+                                </tr>
+                            </table>
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Seccion abajo -->
+    </div>
+</body>
+</html>
